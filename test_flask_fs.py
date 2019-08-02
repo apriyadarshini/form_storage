09645935025c67348641920501fa5817de9c61d5
Aruna Priyadarshini <arunapriyadarshini5@gmail.com>
2019-08-02 17:45:26 +0100

    minor

--- test_flask_fs.py
+++ test_flask_fs.py
@@ -11,18 +11,25 @@ import json
 def test_store(client):
     assert client.post(url_for('forms.store'),data=json.dumps(dict(form_field="xyz",value="xyzsda",localisable="Yes"))).status_code == 200
     
+def test_store_bad(client):
+    assert client.post(url_for('forms.store'),data=json.dumps(dict(value="xyzsda",localisable="Yes"))).status_code == 400
+    
 
 def test_display(client):
     assert client.get(url_for('forms.display')).status_code == 200
     
 def test_display_args1(client):
-    assert client.get(url_for('forms.display'),data=json.dumps(dict(value="xyzsda"))).status_code == 200
+    assert client.post(url_for('forms.display'),data=json.dumps(dict(value="xyzsda"))).status_code == 200
     
     
 def test_display_args2(client):
-    assert client.get(url_for('forms.display'),data=json.dumps(dict(localisable="Yes"))).status_code == 200
+    assert client.post(url_for('forms.display'),data=json.dumps(dict(localisable="Yes"))).status_code == 200
+    
+def test_display_args2_bad(client):
+    assert client.post(url_for('forms.display'),data=json.dumps(dict(localisable=1))).status_code == 400
        
 
 def test_display_getform(client):
     assert client.get(url_for('forms.storedid',id=1)).status_code == 200
-       
+    
+
